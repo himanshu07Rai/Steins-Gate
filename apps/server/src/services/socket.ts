06e8511ddac0c380d23bdd11215fbffcdaf596e0
himanshu07Rai <raihimanshu22345@gmail.com>
2024-10-10 11:53:49 +0530

    [Socket IO Admin]

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -2,6 +2,7 @@ import { Server as SocketIOServer, Socket } from 'socket.io';
 import http from 'http';
 import Redis from 'ioredis';
 import { produceMessage } from "./kafka";
+import { instrument } from "@socket.io/admin-ui"
 
 const pub = new Redis({
   host: 'localhost',
@@ -22,11 +23,16 @@ class SocketServer {
       console.log('Socket server constructor')
       this.io = new SocketIOServer(httpServer,{
         cors: {
-          origin: '*',
+          origin: ['http://localhost:3000', 'https://admin.socket.io'],
+          credentials: true,
           allowedHeaders: '*',
         }
       });
       sub.subscribe("MESSAGES");
+      instrument(this.io, {
+        auth: false,
+        mode:"development"
+       });
     }
 
     public initListeners() {
