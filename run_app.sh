ab3a64a6bb408c820c55ed74d023352d9a4e5f50
Himanshu Rai <himanshu.rai@front.page>
2024-10-14 01:33:29 +0530

    Added next install in initial script and removed container's storage

--- run_app.sh
+++ run_app.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-
-# Step 1: Run Docker Compose
 echo "Starting Docker Compose..."
 docker-compose up -d
-
-# Step 2: Navigate to the web directory
+npm install
 cd apps/web
-
-# Step 3: Build the Next.js app
 echo "Building the web app..."
 npm run build
-
-# Step 4: Start the web app
 echo "Starting the web app..."
 npm run start
